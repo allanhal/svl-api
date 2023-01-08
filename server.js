0c9fbb843d74af9fb969d4c4d26c1657ef5038fd
Allan Pinheiro <allan.hal@gmail.com>
2023-01-08 20:18:55 -0300

    removendo banco de dados

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 10000
-const db = require('./config/db')
+const express = require("express");
+const app = express();
+const port = 10000;
+const db = require("./config/db");
 
-const routes = require('./routes/index')
-routes(app)
+const routes = require("./routes/index");
+routes(app);
 
-db.on('error', console.log.bind(console,'Erro ao conectar com o banco'))
-db.once('open', ()=> {
-  console.log('conexão realizada com sucesso')
-})
-
-
+// db.on("error", console.log.bind(console, "Erro ao conectar com o banco"));
+// db.once("open", () => {
+//   console.log("conexão realizada com sucesso");
+// });
 
 /* app.get('/', (req, res) => {
   res.send('servidor online')
@@ -20,10 +18,9 @@ db.once('open', ()=> {
 // app.get('/livros', (req, res) => {
 //   livros.find((error, livros) => {
 //     res.status(200).json(livros)
-//   }) 
+//   })
 // })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${5000}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
